7d8d1746e90263e01f56f612d5f688b12d9c4963
Adrian Zgorzałek <a.zgorzalek@gmail.com>
2012-02-09 21:01:40 +0100

    fullscreenbg: Remove unnecessary import

--- plugins/fullscreenbg/fullscreenbg.py
+++ plugins/fullscreenbg/fullscreenbg.py
@@ -17,7 +17,7 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-from gi.repository import GObject, Gdk, Gio, Gtk, Eog, Peas, PeasGtk
+from gi.repository import GObject, Gdk, Gio, Gtk, Eog, PeasGtk
 from os.path import join
 
 
